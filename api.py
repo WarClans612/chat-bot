3ff1d66fa70b264220d56a45ef303b9901f8eacb
gunarto90 <gunarto.nctu@gmail.com>
2016-11-16 19:47:05 +0800

    Added qa function in the webserver

--- api.py
+++ api.py
@@ -10,10 +10,6 @@ from bot import bot_function as bot
 
 app = Eve()
 
-@app.route('/')
-def index():
-    return hello_world()
-
 @app.route('/welcome')
 @app.route('/hello')
 def hello_world():
@@ -22,7 +18,7 @@ def hello_world():
     Please use the following route to access the features
     /qa/<question> : Asking question
     """
-    print(welcome)
+    # print(welcome)
     return welcome
 
 @app.route('/find')
@@ -34,10 +30,7 @@ def find_keywords():
 
 @app.route('/qa/<question>')
 def qa_handling(question):
-    # print(answers)
-    # print(keyword_set)
-    # answer = bot.qa_answering(question, answers, keyword_set)
-    answers = 'a'
+    answer = bot.qa_answering(question, answers, keyword_set)
     return jsonify({'question':question, 'answer':answer})
 
 if __name__ == '__main__':
@@ -48,8 +41,6 @@ if __name__ == '__main__':
     stop_words_filename = 'bot/extra_dict/stop_words.txt'
     idf_filename = 'bot/extra_dict/idf.txt.big'
     keywords, keyword_set, num_of_keyword = bot.init_jieba(stop_words_filename, idf_filename, questions)
-    # print(answers)
-    # print(keyword_set)
 
     ### Eve webserver
     app.run(port=PORT, host='0.0.0.0', debug=DEBUG)
