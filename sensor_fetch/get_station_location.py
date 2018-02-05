8145da2d3cb4c3baa01b0208d4785f44a63cd75b
Billy Su <g4691821@gmail.com>
2018-02-05 10:35:59 +0800

    Refactor get_station_location.py

--- sensor_fetch/get_station_location.py
+++ sensor_fetch/get_station_location.py
@@ -7,27 +7,24 @@ from sensor_config import *
 def grab_pm25_station():
     url = 'http://opendata.epa.gov.tw/ws/Data/AQXSite/?$format=json'
     with urllib.request.urlopen(url) as response:
-        data_list = json.loads(response.read().decode('utf-8'))
-    
-    return data_list
+        raw_data = json.loads(response.read().decode('utf-8'))
+    return raw_data
     
 def grab_uvi_station():
     url = 'http://opendata.epa.gov.tw/ws/Data/UV/?$format=json'
     with urllib.request.urlopen(url) as response:
-        data_list = json.loads(response.read().decode('utf-8'))
-    
-    return data_list
+        raw_data = json.loads(response.read().decode('utf-8'))
+    return raw_data
 
 def get_pm25_station_location():
-    data_list = grab_pm25_station()
-    print(data_list)
-    new_data_list = []
-    for i in data_list:
+    raw_data = grab_pm25_station()
+    pm25_station_data = []
+    for i in raw_data:
         data = {}
         data['Lon'] = float(i['TWD97Lon'])
         data['Lat'] = float(i['TWD97Lat'])
         data['SiteName'] = i['SiteName']
-        new_data_list.append(data)
+        pm25_station_data.append(data)
     
     db_url = "{host}:27017".format(host=mongo_host)
     db_name = 'bot'
@@ -35,20 +32,19 @@ def get_pm25_station_location():
     db = client[ 'bot']
     db.drop_collection('pm25_station_data')
     collect = db['pm25_station_data']
-    collect.insert(new_data_list)
+    collect.insert(pm25_station_data)
     
 def get_uvi_station_location():
-    data_list = grab_uvi_station()
-    print(data_list)
-    new_data_list = []
-    for i in data_list:
+    raw_data = grab_uvi_station()
+    uvi_station_data = []
+    for i in raw_data:
         data = {}
-        Lon = i['WGS84Lon']
-        Lat = i['WGS84Lat']
-        data['Lon'] = float(Lon.split(",")[0])+float(Lon.split(",")[1])/100+float(Lon.split(",")[2])/10000
-        data['Lat'] = float(Lat.split(",")[0])+float(Lat.split(",")[1])/100+float(Lat.split(",")[2])/10000
+        Lon = i['WGS84Lon'].split(',')
+        Lat = i['WGS84Lat'].split(',')
+        data['Lon'] = float(Lon[0]) + float(Lon[1])/100 + float(Lon[2])/10000
+        data['Lat'] = float(Lat[0]) + float(Lat[1])/100 + float(Lat[2])/10000
         data['SiteName'] = i['SiteName']
-        new_data_list.append(data)
+        uvi_station_data.append(data)
     
     db_url = "{host}:27017".format(host=mongo_host)
     db_name = 'bot'
@@ -56,9 +52,8 @@ def get_uvi_station_location():
     db = client[ 'bot']
     db.drop_collection('uvi_station_data')
     collect = db['uvi_station_data']
-    collect.insert(new_data_list)
+    collect.insert(uvi_station_data)
 
-    
 if __name__ == "__main__":
     get_pm25_station_location()
     get_uvi_station_location()
