4260dc677b1730971fd5be0b6028aa09c597719f
Billy Su <g4691821@gmail.com>
2018-02-06 09:55:42 +0800

    Refactor get_weather_regular.py

--- sensor_fetch/get_weather_regular.py
+++ sensor_fetch/get_weather_regular.py
@@ -8,18 +8,34 @@ from pymongo import MongoClient
 
 from sensor_config import *
 
-def parse_json_data(pure_data):
-    location = pure_data['records']['location']
-    Location_list = {}
-    for L in location:
-        Location_list[L['locationName']] = {}
-        for element in L['weatherElement']:
-            for E in element['time']:
-                end_time = datetime.strptime(E['endTime'], "%Y-%m-%d %H:%M:%S")
-                if( end_time not in Location_list[L['locationName']] ):
-                    Location_list[L['locationName']][end_time] = {}
-                Location_list[L['locationName']][end_time][element['elementName']] = E['parameter']['parameterName']
-    return Location_list
+def parse_json_data(raw_data):
+    locations = raw_data['records']['location']
+    raw_weather = {}
+    for location in locations:
+        raw_weather[location['locationName']] = {}
+        for element in location['weatherElement']:
+            for timestamp_entry in element['time']:
+                end_time = datetime.strptime(timestamp_entry['endTime'], "%Y-%m-%d %H:%M:%S")
+                if( end_time not in raw_weather[location['locationName']] ):
+                    raw_weather[location['locationName']][end_time] = {}
+                raw_weather[location['locationName']][end_time][element['elementName']] = timestamp_entry['parameter']['parameterName']
+
+    weather_data = list()
+    for location in raw_weather:
+        location_data = raw_weather[location]
+        for time in location_data:
+            item = location_data[time]
+            data = {}
+            data['locationName'] = location
+            data['endTime'] = time
+            data['startTime'] = time - DT.timedelta(hours=12)
+            data['MaxT'] = int(item['MaxT'])
+            data['MinT'] = int(item['MinT'])
+            data['temperature'] = ( int(item['MaxT']) + int(item['MinT']) )/2
+            data['rainfull_prob'] = int(item['PoP'])
+            data['Wx'] = item['Wx']
+            weather_data.append(data)
+    return weather_data
 
 def grab_data():
 
@@ -29,37 +45,22 @@ def grab_data():
 
     request.add_header( 'Authorization' , weather_token)
     with urllib.request.urlopen(request) as response:
-        pure_data = json.loads(response.read().decode('utf-8'))
-    
-    data_list = parse_json_data(pure_data)
-    
-    new_data_list = []
-    for location in data_list:
-        item_list = data_list[location]
-        for time in item_list:
-            item = item_list[time]
-            #print(item)
-            data = {}
-            data['locationName'] = location
-            data['endTime'] = time
-            data['startTime'] = time - DT.timedelta(hours=12)
-            data['temperature'] = ( int(item['MaxT']) + int(item['MinT']) )/2
-            data['rainfull_prob'] = int(item['PoP'])
-            data['Wx'] = item['Wx']
-            new_data_list.append(data)
+        raw_data = json.loads(response.read().decode('utf-8'))
+
+    weather_data = parse_json_data(raw_data)
     
     db_url = "{host}:27017".format(host=mongo_host)
     db_name = 'bot'
     client = MongoClient(db_url,  27017)
     db = client['bot']
-    collect = db['weather_data']
-    for data in new_data_list:
+    collect = db['raw_weather']
+    for data in weather_data:
         upflag = collect.find_one_and_update({'endTime':data['endTime'],"locationName":data['locationName']}, { '$set' : { "temperature": data['temperature'], "rainfull_prob": data['rainfull_prob'], "Wx": data['Wx']}  }  )
         if upflag == None:
             collect.insert_one(data)
-    return data_list
+    return weather_data
     
 if __name__ == "__main__":
-    data_list = grab_data()
-    print (data_list["新竹市"])
+    weather_data = grab_data()
+    print (weather_data["新竹市"])
 
